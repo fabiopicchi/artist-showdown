63c5664b194b1ba8bf9cb342d9ca98b7b5770e6a
fabiopicchi <fabio.exe@gmail.com>
2014-04-12 22:41:36 -0300

    Image wrapper now supports scale and rotation

--- image.lua
+++ image.lua
@@ -11,11 +11,13 @@ Image = utils.inheritsFrom (referrer.Referrer, function (self, file)
     self.img = love.graphics.newImage (file)
     self.width = self.img:getWidth()
     self.height = self.img:getHeight()
+    self.rotation = 0
+    self.scale = {x = 1, y = 1}
 end)
 
 function Image:draw ()
      love.graphics.reset()
-     love.graphics.draw (self.img, self.reference.x + self.x, self.reference.y + self.y)
+     love.graphics.draw (self.img, self.reference.x + self.x, self.reference.y + self.y, self.rotation, self.scale.x, self.scale.y)
 end
 
 return image
