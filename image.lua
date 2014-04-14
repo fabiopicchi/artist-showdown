881aad76b0994d9a7158aad99bca484f9db9f34f
fabiopicchi <fabio.exe@gmail.com>
2014-04-13 23:16:36 -0300

    Added visible property

--- image.lua
+++ image.lua
@@ -13,11 +13,14 @@ Image = utils.inheritsFrom (referrer.Referrer, function (self, file)
     self.height = self.img:getHeight()
     self.rotation = 0
     self.scale = {x = 1, y = 1}
+    self.visible = true
 end)
 
 function Image:draw ()
-     love.graphics.reset()
-     love.graphics.draw (self.img, self.reference.x + self.x, self.reference.y + self.y, self.rotation, self.scale.x, self.scale.y)
+    if self.visible then
+        love.graphics.reset()
+        love.graphics.draw (self.img, self.reference.x + self.x, self.reference.y + self.y, self.rotation, self.scale.x, self.scale.y)
+    end
 end
 
 return image
